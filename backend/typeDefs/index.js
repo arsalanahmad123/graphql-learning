c9579b4c35d50d5d3b66828ba6215c8ea40645f4
arsalanahmad123 <arsalanahmad15092003@gmail.com>
2025-01-15 13:27:37 +0500

    project boilerplate setup

---
+++ backend/typeDefs/index.js
@@ -0,0 +1,8 @@
+import { mergeTypeDefs } from "@graphql-tools/merge";
+
+import userTypeDefs from "./user.typeDef.js"
+import transactionTypeDefs from "./transaction.typeDef.js"
+
+const mergedTypeDefs = mergeTypeDefs([userTypeDefs, transactionTypeDefs]);
+
+export default mergedTypeDefs;
