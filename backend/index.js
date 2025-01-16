e0ce6d5b140e30a63af55df9c863d4c6cfb8156a
arsalanahmad123 <arsalanahmad15092003@gmail.com>
2025-01-16 11:46:31 +0500

    Passport configured

--- backend/index.js
+++ backend/index.js
@@ -2,43 +2,87 @@ import express from 'express';
 import http from 'http';
 import cors from 'cors';
 import { configDotenv } from 'dotenv';
-import connectDB from "./db/connectDB.js"
+import connectDB from './db/connectDB.js';
 
+import passport from 'passport';
+import session from 'express-session';
+import ConnectMongoDBSession from 'connect-mongodb-session';
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import { buildContext } from 'graphql-passport';
+import configurePassport from "./passport/passport.config.js"
 
 import mergedResolvers from './resolvers/index.js';
 import mergedTypeDefs from './typeDefs/index.js';
 
-
-
+// Load environment variables from .env file
 configDotenv();
+configurePassport();
+
 const app = express();
 
+// Create an HTTP server using the Express app
 const httpServer = http.createServer(app);
 
+// Create a MongoDB session store
+const MongoDBStore = ConnectMongoDBSession(session);
+
+// Configure the session store with MongoDB URI and collection name
+const store = new MongoDBStore({
+    uri: process.env.MONGO_URI,
+    collection: 'sessions',
+});
+
+// Catch and log errors from the session store
+store.on('error', (error) => console.error(error));
+
+// Configure the Express app to use sessions
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+        cookie:{
+            maxAge: 1000 * 60 * 60 * 24 * 7,// 1 week
+            httpOnly: true,
+        },
+        store
+    })
+);
+
+// Initialize Passport and configure it to use sessions
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Create a new ApolloServer instance with type definitions, resolvers, and plugins
 const server = new ApolloServer({
     typeDefs: mergedTypeDefs,
     resolvers: mergedResolvers,
     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 
+// Start the ApolloServer
 await server.start();
 
+// Apply middleware to the Express app
 app.use(
-  '/',
-  cors(),
-  express.json(),
-  expressMiddleware(server,{
-    context: async ({req}) => ({req})
-  }),
+    '/',
+    cors({
+        origin: 'http://localhost:3000',
+        credentials: true,
+    }), // Enable Cross-Origin Resource Sharing
+    express.json(), // Parse incoming JSON requests
+    expressMiddleware(server, {
+        context: async ({ req }) => buildContext({ req }), // Provide request context to ApolloServer
+    })
 );
 
-await new Promise((resolve) =>
-    {
-        httpServer.listen({ port: 4000 }, resolve);
-        console.log(`🚀 Server ready at http://localhost:4000`);
-    }
-);
+// Start the HTTP server and listen on port 4000
+await new Promise((resolve) => {
+    httpServer.listen({ port: 4000 }, resolve);
+    console.log(`🚀 Server ready at http://localhost:4000`);
+});
+
+// Connect to the MongoDB database
 await connectDB();
---
+++ backend/passport/passport.config.js
@@ -0,0 +1,42 @@
+import passport from "passport";
+
+import bcrypt from "bcryptjs"
+import { User } from "../models/user.model"
+import { GraphQLLocalStrategy } from "graphql-passport";
+
+
+export const configurePassport = async() => {
+    passport.serializeUser((user,done) => {
+        console.log("Serializing user")
+        done(null,user.id)
+    })
+
+    passport.deserializeUser(async(id,done) => {
+        try {
+            console.log("Deserializing user")
+            const user = await User.findById(id)
+            done(null,user)
+        } catch (error) {
+            done(error)
+        }
+    })
+
+    passport.use(new GraphQLLocalStrategy(async (username, password, done) => {
+        try {
+            const user = await User.findOne({username})
+            if(!user){
+                throw new Error("Invalid username or password");
+            }
+
+            const isValid = await bcrypt.compare(password,user.password)
+            if(!isValid){
+                throw new Error("Invalid username or password");
+            }
+
+            return done(null,user)
+
+        } catch (error) {
+            return done(error);
+        }
+    }))
+}
